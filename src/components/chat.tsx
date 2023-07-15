563473506cad28d95bf44e550308e4d4206f96cd
Kuzey Kose <kosekuzey97@gmail.com>
2023-07-15 11:54:57 -0400

    Hotfix

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -15,21 +15,21 @@ export default function Chat() {
     <div className="relative max-w-2xl mx-auto">
       <div className="h-screen pt-10 space-y-4">
         <div className="border p-4 rounded">
-          <h2 className="text-xl">👋🏼 Selam</h2> <br /> Bu chatbot'u canlı
+          <h2 className="text-xl">👋🏼 Selam</h2> <br /> Bu chatbot&apos;u canlı
           yayında yaptık! Bu chatbot sadece <strong>SPOR</strong> üzerine
           çalışıryor. Bu konu hakkında istediğin her şeyi sorabilirsin!
         </div>
         <div className="h-[600px] overflow-auto">
           <ul className="space-y-2">
-            {messages.map((item) =>
+            {messages.map((item, index) =>
               item.role == "user" ? (
-                <li className="flex justify-end text-green-500 ">
+                <li className="flex justify-end text-green-500" key={index}>
                   <p className="border border-gray-200 rounded-md p-4">
                     {item.content}
                   </p>
                 </li>
               ) : (
-                <li>
+                <li key={index}>
                   <article className="prose">
                     <div className="border border-gray-200 rounded-md p-4">
                       <ReactMarkdown>{item.content}</ReactMarkdown>
